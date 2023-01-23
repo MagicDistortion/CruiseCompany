b749037be4171dd32aeeac53a8cc35597dc6276c
Magicdistortion <Magicdistortion@gmail.com>
2023-01-23 02:56:28 +0200

    updates

--- src/main/java/services/EncodePassword.java
+++ src/main/java/services/EncodePassword.java
@@ -11,23 +11,17 @@ public class EncodePassword {
     private static final Logger LOGGER = Logger.getLogger(EncodePassword.class);
 
     public String getHashPassword(String input) {
-
-        MessageDigest messageDigest;
         byte[] byteDigest = new byte[0];
-
         try {
-            messageDigest = MessageDigest.getInstance("MD5");
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
             messageDigest.reset();
             byteDigest = messageDigest.digest(input.getBytes());
         } catch (NoSuchAlgorithmException e) {
             LOGGER.error("failed to Encode password", e);
         }
-
         BigInteger number = new BigInteger(1, byteDigest);
         StringBuilder hashHex = new StringBuilder(number.toString(16));
-        while (hashHex.length() < 32) {
-            hashHex.insert(0, "0");
-        }
+        while (hashHex.length() < 32) hashHex.insert(0, "0");
         return hashHex.toString();
     }
 }

ae702e5e7d65430a82f808a08ff137334dad815f
Magicdistortion <Magicdistortion@gmail.com>
2023-01-25 13:14:02 +0200

    updates

--- src/main/java/commands/DownloadTicketCommand.java
+++ src/main/java/commands/DownloadTicketCommand.java
@@ -1,5 +1,7 @@
 package commands;
 
+import dao.TicketsDAO;
+import models.tickets.Ticket;
 import services.TicketDownloadManager;
 
 import javax.servlet.http.HttpServletRequest;
@@ -7,16 +9,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DownloadTicketCommand implements Command {
+    private final TicketsDAO ticketsDAO = new TicketsDAO();
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Ticket ticket = ticketsDAO.findTicketById(Integer.parseInt(request.getParameter("ticket_id")));
+        String fileName = ticket.getCruiseName() + ticket.getId()+".pdf";
+        response.setHeader("Content-Type", "application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+
         TicketDownloadManager ticketDownloadManager = new TicketDownloadManager();
-        ticketDownloadManager.run(request);
-        response.sendRedirect("my_profile");
+        ticketDownloadManager.run(ticket, response);
     }
 
     @Override
     public boolean canHandle(String uri, String method) {
-        return uri.equalsIgnoreCase("passenger/download_ticket")&&method.equalsIgnoreCase("Get");
+        return uri.equalsIgnoreCase("passenger/download_ticket") && method.equalsIgnoreCase("Get");
     }
 }
--- src/main/java/services/TicketDownloadManager.java
+++ src/main/java/services/TicketDownloadManager.java
@@ -2,7 +2,11 @@ package services;
 
 import java.io.File;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.util.Objects;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
@@ -11,22 +15,18 @@ import models.tickets.Ticket;
 import org.apache.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class TicketDownloadManager {
     private final static Logger logger = Logger.getLogger(TicketDownloadManager.class);
-    private final TicketsDAO ticketsDAO = new TicketsDAO();
 
-    public void run(HttpServletRequest request) {
+    public void run(Ticket ticket, HttpServletResponse response) {
         try {
-            Ticket ticket = ticketsDAO.findTicketById(Integer.parseInt(request.getParameter("ticket_id")));
             Document document = new Document();
-            OutputStream outputStream = Files.newOutputStream(new File(
-                    "C:\\Users\\Magic\\OneDrive\\Рабочий стол\\" + ticket.getCruiseName() + ticket.getId() + ".pdf").toPath());
-            PdfWriter.getInstance(document, outputStream);
-
+            PdfWriter.getInstance(document, response.getOutputStream());
             document.open();
 
-            Font font = new Font(BaseFont.createFont("c:/Windows/Fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED));
+            Font font = new Font(BaseFont.createFont("pdf/arial.ttf",BaseFont.IDENTITY_H, BaseFont.EMBEDDED));
             PdfPTable pdfPTable = new PdfPTable(4);
             PdfPTable pdfPTable2 = new PdfPTable(1);
             PdfPTable pdfPTable4 = new PdfPTable(1);
@@ -42,9 +42,11 @@ public class TicketDownloadManager {
             pdfPTable.addCell(new PdfPCell(new Paragraph(String.valueOf(ticket.getDuration()))));
             pdfPTable2.addCell(new PdfPCell(new Paragraph("Ласкаво просимо на круїз '"
                     + ticket.getCruiseName() + "', приємної подорожі", font)));
-            pdfPTable3.addCell(Image.getInstance("image/qr-code.jpeg"));
-            pdfPTable3.addCell(Image.getInstance("image/ship.jpeg"));
-            pdfPTable4.addCell(Image.getInstance("image/back.jpeg"));
+
+            pdfPTable3.addCell(Image.getInstance(getUrl("pdf/qr-code.jpeg")));
+            pdfPTable3.addCell(Image.getInstance(getUrl("pdf/ship.jpeg")));
+            pdfPTable4.addCell(Image.getInstance(getUrl("pdf/back.jpeg")));
+
 
             document.add(pdfPTable);
             document.add(pdfPTable2);
@@ -52,10 +54,13 @@ public class TicketDownloadManager {
             document.add(pdfPTable4);
 
             document.close();
-            outputStream.close();
         } catch (Exception e) {
             logger.error("failed to create ticket in PDF", e);
             throw new RuntimeException(e);
         }
+    }
+
+    private URL getUrl(String resourceFile) {
+        return Objects.requireNonNull(getClass().getClassLoader().getResource(resourceFile));
     }
 }
